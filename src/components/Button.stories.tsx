2435d72c9cfd75db8bf95908ccf986c720ef11ff
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-10-12 18:59:12 -0300

    feat: Add stories

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  args: {
+    children: "Lorem Ipsum",
+  },
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {};
